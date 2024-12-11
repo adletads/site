4ae0836b831fb370074afe1f03064a4c14645172
adletads <adletadvertisements@gmail.com>
2024-12-11 13:20:39 +0530

    Sixth commit

--- src/data/team.ts
+++ src/data/team.ts
@@ -4,13 +4,13 @@ export const team = [
     name: 'Jinu Joy',
     role: 'Founder & CEO',
     bio: 'Visionary leader with over 3 years of experience in advertising and marketing.',
-    image: 'https://images.unsplash.com/photo-1560250097-0b93528c311a?auto=format&fit=crop&q=80&w=400'
+    image: 'https://i.imghippo.com/files/kRG2242Kg.jpg'
   },
   {
     id: '2',
     name: 'Sonu S',
     role: 'Co Founder & CTO',
     bio: 'Award-winning designer with a passion for creating impactful advertising campaigns.',
-    image: 'https://i.imghippo.com/files/TdMe8904fNM.jpg'
+    image: 'https://i.imghippo.com/files/puCo1151yw.jpg'
   }
 ];
